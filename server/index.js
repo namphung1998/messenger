9f8bbd46c1e28315a82f3d3db40e80210e8c5c2c
Nam Phung <nphung@macalester.edu>
2018-11-30 15:40:19 -0600

    working on models

--- server/index.js
+++ server/index.js
@@ -5,64 +5,27 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const expressGraphQL = require('express-graphql');
 
-const schema = require('./schema/schema');
 const User = require('./models/user');
-const Conversation = require('./models/conversation');
-const Text = require('./models/text');
 
 const MONGO_URI = 'mongodb://nphung:password1@ds049558.mlab.com:49558/messenger';
 mongoose.connect(MONGO_URI);
-
-// const user1 = User.findById('5bfcbd003ce7122cd98001a2').then(res => res);
-// const user2 = User.findById('5bfcbcf83ce7122cd98001a1').then(res => res);
-// Promise.all([user1, user2]).then(users => {
-//   const chat = new Conversation({ users, texts: []});
-//   users.forEach(user => {
-//     user.chats.push(chat);
-//     user.save();
-//   })
-//   chat.save();
-// });
-
-
-// User.findById('5bfcbd003ce7122cd98001a2')
-//   .then(res => {
-    
-//     Conversation.findById('5bfdce7bc6c88c10ba9c190c')
-//       .then(chat => {
-//         const text = new Text({ content: 'Hello again', sender: res });
-//         chat.texts.push(text);
-//         chat.save();
-//         text.save();
-//       })
-//   })
-
-
-// Conversation.findById('5bfdce7bc6c88c10ba9c190c')
-//   .populate('texts')
-//   .then(res => {
-//     const text = new Text({ content: 'Hello' });
-//     res.texts.push(text);
-//     res.save();
-//     text.save();
-//   })
-
-Text.findSender('5bfdd117f581cc118f26a59f').then(res => console.log(res));
-
-
-
 
 const app = express();
 
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*' }));
-app.use('/graphql', expressGraphQL({
-  schema,
-  graphiql: true
-}));
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello!<h1>')
+});
+
+app.get('/users', (req, res) => {
+  User.find({}, function(err, users) {
+    if (err) throw err;
+    
+    return res.json(users);
+
+  });
 });
 
 app.post('/users/create', (req, res) => {
@@ -72,7 +35,7 @@ app.post('/users/create', (req, res) => {
   const user = new User({ name, email, password });
 
   user.save();
-  res.end();
+  res.redirect('/users');
 });
 
 const server = http.createServer(app);
